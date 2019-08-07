b2213f1a6ab9f3ad4486535fde9edb444112443c
wackyJ <785273094@qq.com>
2019-08-07 23:32:40 +0800

    添加登录路由，ajax（使用restful风格）测试前后端数据传输，成功传输数据

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+//添加登录路由
+router.get("/v1/login/:uname&:upwd",(req,res)=>{
+	//获取提交数据
+	var $uname=req.params.uname;
+	var $upwd=req.params.upwd;
+	console.log($uname);
+	console.log($upwd);
+	//执行mongodb语句
+
+});
+
 
 module.exports = router;
