90e17051ce5a7430d56bd929c8ea1bd5ffc16099
wackyJ <785273094@qq.com>
2019-08-29 22:14:57 +0800

    增加库存页面移除按钮接口

--- routes/product.js
+++ routes/product.js
@@ -10,17 +10,29 @@ router.get("/v1/repertory",(req,res)=>{
     pno:req.query.pno||0,
     data:[]
   };
-  var sql="select * from wh_product";
+  var sql="SELECT * FROM wh_product";
   query(sql,[])
   .then(result=>{
     output.count=result.length;
     output.pageCount=Math.ceil(output.count/output.pageSize);
-    sql+=` limit ?,?`;
+    sql+=` LIMIT ?,?`;
     return query(sql,[output.pageSize*output.pno,output.pageSize]);
   })
   .then(result=>{
     output.data=result;
     res.send(output);
+  })
+});
+
+router.post("/v1/delRecord",(req,res)=>{
+  let _pid=req.body.params.pid;
+  let sql=`DELETE FROM wh_product WHERE pid = ?;`;
+  query(sql,[_pid]).then(result=>{
+    if(result.affectedRows>0){
+			res.send({code:1,msg:"delete success"});
+		}else{
+			res.send({code:-1,msg:"delect faile"});
+		}
   })
 })
 
