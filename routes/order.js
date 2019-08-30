a22bb090f763629468663558c01a1b8ba7f0f48c
wackyJ <jdd@email.com>
2019-08-30 17:39:43 +0800

    order.js修改

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const pool = require("../pool");
+const query = require("./query");
 const router = express.Router();
 
 router.post("/v1/createOrder",(req,res)=>{
-  console.log(req.body);
-  var $sizeForm = req.body.params.zxc;
-  console.log($sizeForm);
-  res.send("这是后台数据")
+  var $orderForm = req.body.params.orderForm;
+  var $orderProduct = $orderForm.orderProduct;
+  delete $orderForm.orderProduct;
+  $orderForm.create_date=new Date($orderForm.create_date).getTime();
+  $orderForm.delivery_date=new Date($orderForm.delivery_date).getTime();
+  console.log($orderForm);
+  console.log($orderProduct);
+  var sql="INSERT INTO wh_order Value (?,?)";
+  if($orderProduct.length<=1){
+    query(sql,[ $orderForm,orderProduct[0] ]).then(result=>{
+       res.send({code:200,msg:"success",data:{result}})
+    })
+  }else{
+    for(var i=0;i<$orderProduct.length;i++){
+
+    }
+  }
 })
 
 module.exports = router;
