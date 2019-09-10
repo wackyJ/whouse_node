6da9b319b53d0cb597fde9e3c48105ae051dfd47
wackyJ <785273094@qq.com>
2019-09-10 16:10:31 +0800

    完善订单查询页面的接口

--- routes/order.js
+++ routes/order.js
@@ -3,9 +3,11 @@ const query = require("./query");
 const router = express.Router();
 //所有订单信息（包含订单详情查询）
 router.get("/v1/allOrders",(req,res)=>{
+  let sql="select * from wh_order,wh_order_detail where wh_order.onum=wh_order_detail.onum;";
   query(sql,[]).then(result=>{
     if(result.length>0){
-      
+      // console.log(result);
+      res.send({code:200,msg:"success",data:result});
     }
   })
 })
@@ -44,15 +46,6 @@ router.post("/v1/OrderSubmission",(req,res)=>{
     res.send({code:200,msg:"success"});
   })
 });
-//订单查询
-router.get("/v1/OrderSearch",(req,res)=>{
-  //判断用户是否已经登录
-  let uid = req.session.uid;
-  if(!uid){
-    res.send({code:-1,mgs:"请先登录"})
-    return;
-  }
-})
 //订单商品单价显示
 router.get("/v1/unitPrice",(req,res)=>{
   let $pid=req.query.pid;
@@ -63,6 +56,41 @@ router.get("/v1/unitPrice",(req,res)=>{
     }else{
       res.send({code:201,msg:"The target product does not exist"})
     }
+  })
+})
+//订单查询
+router.get("/v1/search",(req,res)=>{
+  //判断用户是否已经登录
+  let uid = req.session.uid;
+  if(!uid){
+    res.send({code:-1,mgs:"请先登录"})
+    return;
+  }
+  let output={
+    count:0,
+    pageSize:9,
+    pageCount:0,
+    pno:req.query.pno||0,
+    data:[]
+  };
+  let kw=req.query.kw;
+  let sql="SELECT * FROM wh_order_detail INNER JOIN wh_order ON wh_order_detail.onum=wh_order.onum WhERE wh_order.onum=? ";
+  query(sql,[kw])
+  .then(result=>{
+    output.count=result.length;
+    output.pageCount=Math.ceil(output.count/output.pageSize);
+    sql+=` limit ?,?`;
+    console.log(output.pageSize);
+    return query(sql,[kw,output.pageSize*output.pno,output.pageSize]);
+  })
+  .then(result=>{
+    console.log(2);
+    console.log(result);
+    output.data=result;
+    res.send(output);
+  }).catch(err=>{
+    console.log(3);
+    console.log(err);
   })
 })
 
