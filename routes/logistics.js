a45173289b35f04151bbcb79de12699b859da0f9
wackyJ <785273094@qq.com>
2019-09-20 08:38:02 +0800

    修复物流即时查询接口bug

--- routes/logistics.js
+++ routes/logistics.js
@@ -11,17 +11,17 @@ router.get("/v1/immediateQuery",(req,res)=>{
     var paramData ={ShipperCode,LogisticCode}
     var b = Buffer.from(md5(JSON.stringify(paramData)+"0ae25830-610b-4457-ab3d-e065586a4081"),'utf-8');
     // console.log(b);
-    axios.post("http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx",{
+    axios.get("http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx",{
     params:{
         RequestData:urlencode(JSON.stringify(paramData)),
         EBusinessID: 1579500,
         RequestType: 1002,
         DataSign: urlencode(b.toString('base64'))
     }}).then(result=>{
-    // console.log(result.data);
+    console.log(result.data);
     res.send({code:200,msg:"success",data:result.data})
     }).catch(err=>{
-    // console.log(err);
+    console.log(err);
     res.send({code:201,msg:"err"})
     })
     // console.log("end");
@@ -81,7 +81,6 @@ router.post("/v1/logisticsTracking",(req,res)=>{
     })
     // console.log("end");
 })
-
 //物流追踪(轨迹接收) 每次全量推送，一次最多推送 10 单
 router.post("/v1/pathPush",(req,res)=>{
     //需按快递鸟要求开发接口，保证信息的正常接收。必须在 5S 内对快递鸟请求做出响应(数据存储后马上返回响应结果，然后再处理数据)，响应结果按接口格式要求返回。
